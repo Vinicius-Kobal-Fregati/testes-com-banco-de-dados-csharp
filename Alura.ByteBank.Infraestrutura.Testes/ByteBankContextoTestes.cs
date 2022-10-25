993358fa3913357c542ffa5992f7bd2dc5412410
Vinícius Kobal Fregati <vfregati@gmail.com>
2022-10-25 17:57:20 -0300

    Criacao do primeeiro teste de integracao, realizacao e aplicacao de migracao e aplicacao da injecao de dependencia para realizar a inversao de dependencia

---
+++ Alura.ByteBank.Infraestrutura.Testes/ByteBankContextoTestes.cs
@@ -0,0 +1,34 @@
+﻿using Alura.ByteBank.Dados.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public class ByteBankContextoTestes
+    {
+        [Fact]
+        public void TestaConexaoContextoComDBMySQL()
+        {
+            //Arrange
+            var contexto = new ByteBankContexto();
+            bool conectado;
+
+            //Act
+            try
+            {
+                conectado = contexto.Database.CanConnect(); // Retorna um booleano se consegue se conectar
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Não foi possível conectar a base de dados");
+            }
+
+            //Assert
+            Assert.True(conectado);
+        }
+    }
+}
